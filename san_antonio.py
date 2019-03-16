dab20fcfc1bfb84c037ec57f4ad5d372e83f1f9f
Staub Guillaume <staubguillaume@macbook-pro-de-vympel.home>
2019-03-16 12:10:28 +0100

    Création des deux listes contenant les citations (quotes) et les personnages (charachters)

---
+++ san_antonio.py
@@ -0,0 +1,2 @@
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes!","On doit pouvoir choisir entre s'écouter parler et se faire entendre"]
+charachters =["alvin et les Chipmunks","Babar","betty boop","calimero",'casper',"le chat potté","kirikou"]
